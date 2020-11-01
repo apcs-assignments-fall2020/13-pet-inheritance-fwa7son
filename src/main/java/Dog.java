8fda2684eb70cede5ff56a3fb25211e62b49ecb7
fwa7son <21fwatson@athenian.org>
2020-11-01 21:32:55 +0000

    completed dog method and client driver thing

--- src/main/java/Dog.java
+++ src/main/java/Dog.java
@@ -1,18 +1,37 @@
 public class Dog extends Pet {
     // Instance variable(s)
-
+    private String breed;
 
     // Constructors
-    
+    public Dog(String name, int age, String breed){
+        super(name,age);
+        this.breed = breed;
+    }
 
+    public Dog(){
+        super("Duffy",4);
+        this.breed = "Golden Doodle";
+    }
     // makeNoise() method
-
+    public void makeNoise(){
+        System.out.println("woof");
+    }
 
     // toString method
-
+    public String toString() {
+        String str = "Name:" + this.getName() + " Age:" + this.getAge() + " Breed:" + this.breed;
+        return str;
+    }
 
     // Getter
-
+    public String getBreed(){
+        return this.breed;
+    }
 
     // Setter
+    public void setBreed(String breed){
+        if (breed.trim().length() != 0) {
+        this.breed = breed;
+        }
+    }
 }
--- src/main/java/DogClient.java
+++ src/main/java/DogClient.java
@@ -1,5 +1,7 @@
 public class DogClient {
     public static void main(String[] args) {
-        
+        Dog duffy = new Dog("Duffy", 3, "golden doodle");
+        System.out.println(duffy);
+        duffy.makeNoise();        
     }
 }
